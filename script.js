a3da7bb022f92c6014a3f0dc23da61494c2c671d
Rijkaard <harrison.rijkaard@gmail.com>
2018-11-12 18:32:41 -0600

    javascript

---
+++ script.js
@@ -0,0 +1,29 @@
+// JavaScript source code
+
+
+//FUNCTION
+
+function request() {
+    var httpRequest = new XMLHttpRequest(); 
+    var url = "https://info2180-lab6-rijkaa.c9users.io/request.php?q=rick";
+
+    httpRequest.onreadystatechange = doSomething;
+    httpRequest.open('GET', url);
+    httpRequest.send();
+
+}
+
+//END FUNCION
+
+window.onload = function{
+    $("#btn").onclick = function{
+        //request() ;
+        var httpRequest = new XMLHttpRequest();
+        var url = "https://info2180-lab6-rijkaa.c9users.io/request.php?q=rick";
+
+        httpRequest.onreadystatechange = doSomething;
+        httpRequest.open('GET', url);
+        httpRequest.send();
+
+    };
+};
